35ba2411ae69758ad9e88e68063fd469bee151d2
Boumlik Brahim <boumlik01brahim@gmail.com>
2025-12-23 15:59:00 +0100

    refactor order processing logic for improved readability and maintainability

--- src/controllers/my-controller.ts
+++ src/controllers/my-controller.ts
@@ -1,87 +1,65 @@
-/* eslint-disable @typescript-eslint/switch-exhaustiveness-check */
-/* eslint-disable max-depth */
-/* eslint-disable no-await-in-loop */
-import {eq} from 'drizzle-orm';
+
 import fastifyPlugin from 'fastify-plugin';
-import {serializerCompiler, validatorCompiler, type ZodTypeProvider} from 'fastify-type-provider-zod';
+import {type Cradle} from '@fastify/awilix';
 import {z} from 'zod';
-import {orders, products} from '@/db/schema.js';
+import {serializerCompiler, validatorCompiler, type ZodTypeProvider} from 'fastify-type-provider-zod';
+import {eq} from 'drizzle-orm';
+import {orders} from '@/db/schema.js';
+import {type ProductService} from '@/services/impl/product.service.js';
 
 export const myController = fastifyPlugin(async server => {
-	// Add schema validator and serializer
 	server.setValidatorCompiler(validatorCompiler);
 	server.setSerializerCompiler(serializerCompiler);
 
-	server.withTypeProvider<ZodTypeProvider>().post('/orders/:orderId/processOrder', {
-		schema: {
-			params: z.object({
-				orderId: z.coerce.number(),
-			}),
+	server.withTypeProvider<ZodTypeProvider>().post(
+		'/orders/:orderId/processOrder',
+		{
+			schema: {
+				params: z.object({
+					orderId: z.coerce.number(),
+				}),
+			},
 		},
-	}, async (request, reply) => {
-		const dbse = server.diContainer.resolve('db');
-		const ps = server.diContainer.resolve('ps');
-		const order = (await dbse.query.orders
-			.findFirst({
+		async (request, reply) => {
+			const database: Cradle['db'] = server.diContainer.resolve('db');
+			const ps: ProductService = server.diContainer.resolve('ps');
+
+			const order = await database.query.orders.findFirst({
 				where: eq(orders.id, request.params.orderId),
 				with: {
-					products: {
-						columns: {},
-						with: {
-							product: true,
-						},
-					},
+					products: {columns: {}, with: {product: true}},
 				},
-			}))!;
-		console.log(order);
-		const ids: number[] = [request.params.orderId];
-		const {products: productList} = order;
+			});
 
-		if (productList) {
-			for (const {product: p} of productList) {
-				switch (p.type) {
-					case 'NORMAL': {
-						if (p.available > 0) {
-							p.available -= 1;
-							await dbse.update(products).set(p).where(eq(products.id, p.id));
-						} else {
-							const {leadTime} = p;
-							if (leadTime > 0) {
-								await ps.notifyDelay(leadTime, p);
-							}
-						}
-
-						break;
-					}
-
-					case 'SEASONAL': {
-						const currentDate = new Date();
-						if (currentDate > p.seasonStartDate! && currentDate < p.seasonEndDate! && p.available > 0) {
-							p.available -= 1;
-							await dbse.update(products).set(p).where(eq(products.id, p.id));
-						} else {
-							await ps.handleSeasonalProduct(p);
-						}
-
-						break;
-					}
-
-					case 'EXPIRABLE': {
-						const currentDate = new Date();
-						if (p.available > 0 && p.expiryDate! > currentDate) {
-							p.available -= 1;
-							await dbse.update(products).set(p).where(eq(products.id, p.id));
-						} else {
-							await ps.handleExpiredProduct(p);
-						}
-
-						break;
-					}
-				}
+			if (!order) {
+				return reply.status(404).send({error: 'Order not found'});
 			}
-		}
 
-		await reply.send({orderId: order.id});
-	});
+			await Promise.all(
+				order.products?.map(async ({product}) => {
+					switch (product.type) {
+						case 'NORMAL': {
+							await ps.handleNormalProduct(product);
+							break;
+						}
+
+						case 'SEASONAL': {
+							await ps.handleSeasonalProduct(product);
+							break;
+						}
+
+						case 'EXPIRABLE': {
+							await ps.handleExpiredProduct(product);
+							break;
+						}								
+						default: {
+							throw new Error(`Unhandled product type: ${product.type}`);
+						}
+					}
+				}) || [],
+			);
+
+			return reply.send({orderId: order.id});
+		},
+	);
 });
-
